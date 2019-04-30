284123ba140afe8af15d40bc8f33687a218deafc
Jake Champion <jakechampion.jake2@googlemail.com>
2019-04-30 14:05:09 +0100

    a

--- index.js
+++ index.js
@@ -2,40 +2,6 @@
 
 require("make-promises-safe"); // installs an 'unhandledRejection' handler
 
-const memwatch = require("@airbnb/node-memwatch");
-const start = new Date();
-function msFromStart() {
-	return new Date() - start;
-}
-
-// report to console postgc heap size
-memwatch.on("stats", function(d) {
-	console.log("postgc:", {
-		milliseconds_since_application_stated: msFromStart(),
-		heap_used_in_bytes: d.used_heap_size,
-	});
-});
-
-memwatch.on("leak", function(d) {
-	console.log("LEAK:", d);
-});
-
-// also report periodic heap size (every 10s)
-setInterval(function() {
-	console.log("naive:", {
-		milliseconds_since_application_stated: msFromStart(),
-		memory_usage: process.memoryUsage(),
-	});
-}, 5000);
-
-const process = require("process");
-global.Promise = require("bluebird");
-if (process.env.NODE_ENV !== "production") {
-	Promise.config({
-		longStackTraces: true,
-	});
-}
-
 const dotenv = require("dotenv");
 const service = require("./lib/service");
 const throng = require("throng");
--- test/integration/v3/bundles/css.test.js
+++ test/integration/v3/bundles/css.test.js
@@ -5,173 +5,175 @@ global.Promise = require("bluebird");
 Promise.config({ longStackTraces: true });
 const proclaim = require("proclaim");
 
-test("GET /v3/bundles/css", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css")
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+describe("/v3/bundles/css", function() {
+	it("GET /v3/bundles/css", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css")
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules")
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules")
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=,,", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=,,")
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=,,", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=,,")
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=1a-", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=1a-")
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=1a-", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=1a-")
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.19", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-test-component@1.0.19")
-		.expect(200)
-		.expect(
-			"cache-control",
-			"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-		)
-		.expect("Content-Type", "text/css; charset=utf-8")
-		.expect(response => {
-			proclaim.isString(response.text);
-			proclaim.notMatch(
-				response.text,
-				/\/\*# sourceMappingURL=data:application\/json;base64,(.+)/,
-			);
-		});
-	// TODO: As o-autoinit will be included in the bundle by default, the etag will change whenever a new version of o-autoinit is released.
-	// .expect("etag", "d41d8cd98f00b204e9800998ecf8427e");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.19", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-test-component@1.0.19")
+			.expect(200)
+			.expect(
+				"cache-control",
+				"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+			)
+			.expect("Content-Type", "text/css; charset=utf-8")
+			.expect(response => {
+				proclaim.isString(response.text);
+				proclaim.notMatch(
+					response.text,
+					/\/\*# sourceMappingURL=data:application\/json;base64,(.+)/,
+				);
+			});
+		// TODO: As o-autoinit will be included in the bundle by default, the etag will change whenever a new version of o-autoinit is released.
+		// .expect("etag", "d41d8cd98f00b204e9800998ecf8427e");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.17%20-%201.0.19", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-test-component@1.0.17%20-%201.0.19")
-		.expect(200)
-		.expect(
-			"cache-control",
-			"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-		)
-		.expect("Content-Type", "text/css; charset=utf-8")
-		.expect(response => {
-			proclaim.isString(response.text);
-			proclaim.notMatch(
-				response.text,
-				/\/\*# sourceMappingURL=data:application\/json;base64,(.+)/,
-			);
-		});
-	// TODO: As o-autoinit will be included in the bundle by default, the etag will change whenever a new version of o-autoinit is released.
-	// .expect("etag", "d41d8cd98f00b204e9800998ecf8427e");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.17%20-%201.0.19", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-test-component@1.0.17%20-%201.0.19")
+			.expect(200)
+			.expect(
+				"cache-control",
+				"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+			)
+			.expect("Content-Type", "text/css; charset=utf-8")
+			.expect(response => {
+				proclaim.isString(response.text);
+				proclaim.notMatch(
+					response.text,
+					/\/\*# sourceMappingURL=data:application\/json;base64,(.+)/,
+				);
+			});
+		// TODO: As o-autoinit will be included in the bundle by default, the etag will change whenever a new version of o-autoinit is released.
+		// .expect("etag", "d41d8cd98f00b204e9800998ecf8427e");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.19", async t => {
-	return request(this.app)
-		.get(
-			"/v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.19",
-		)
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.19", async t => {
+		return request(this.app)
+			.get(
+				"/v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.19",
+			)
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.17%20-%201.0.19", async t => {
-	return request(this.app)
-		.get(
-			"/v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.17%20-%201.0.19",
-		)
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.17%20-%201.0.19", async t => {
+		return request(this.app)
+			.get(
+				"/v3/bundles/css?modules=o-test-component@1.0.19,o-test-component@1.0.17%20-%201.0.19",
+			)
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.17,o-test-component@1.0.19", async t => {
-	return request(this.app)
-		.get(
-			"/v3/bundles/css?modules=o-test-component@1.0.17,o-test-component@1.0.19",
-		)
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.17,o-test-component@1.0.19", async t => {
+		return request(this.app)
+			.get(
+				"/v3/bundles/css?modules=o-test-component@1.0.17,o-test-component@1.0.19",
+			)
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=o-autoinit@1.3.3,o-test-component@1.0.29", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-autoinit@1.3.3,o-test-component@1.0.29")
-		.expect(200)
-		.expect("etag", "b012ee3b8ce835fa47c4b09d9d97c6f6")
-		.expect(
-			"cache-control",
-			"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-		)
-		.expect("Content-Type", "text/css; charset=utf-8")
-		.expect(response => {
-			proclaim.isString(response.text);
-			proclaim.match(
-				response.text,
-				/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-autoinit@1\.3\.3,o-test-component@1\.0\.29&shrinkwrap=\n \*\//,
-			);
-		});
-});
+	it("GET /v3/bundles/css?modules=o-autoinit@1.3.3,o-test-component@1.0.29", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-autoinit@1.3.3,o-test-component@1.0.29")
+			.expect(200)
+			.expect("etag", "b012ee3b8ce835fa47c4b09d9d97c6f6")
+			.expect(
+				"cache-control",
+				"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+			)
+			.expect("Content-Type", "text/css; charset=utf-8")
+			.expect(response => {
+				proclaim.isString(response.text);
+				proclaim.match(
+					response.text,
+					/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-autoinit@1\.3\.3,o-test-component@1\.0\.29&shrinkwrap=\n \*\//,
+				);
+			});
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=maybe", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=maybe")
-		.expect(400)
-		.expect("Content-Type", "text/html; charset=utf-8")
-		.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=maybe", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=maybe")
+			.expect(400)
+			.expect("Content-Type", "text/html; charset=utf-8")
+			.expect("cache-control", "max-age=0, must-revalidate, no-cache, no-store");
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=on&shrinkwrap=o-autoinit%401.3.3", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=on")
-		.expect(200)
-		.expect(
-			"cache-control",
-			"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-		)
-		.expect("Content-Type", "text/css; charset=utf-8")
-		.expect("etag", "11b0dac9b84449c938c4dc6759c12945")
-		.expect(response => {
-			proclaim.isString(response.text);
-			proclaim.match(
-				response.text,
-				/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-test-component@1\.0\.29&shrinkwrap=o-autoinit%401\.3\.3\n \*\//,
-			);
-		});
-});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=on&shrinkwrap=o-autoinit%401.3.3", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=on")
+			.expect(200)
+			.expect(
+				"cache-control",
+				"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+			)
+			.expect("Content-Type", "text/css; charset=utf-8")
+			.expect("etag", "11b0dac9b84449c938c4dc6759c12945")
+			.expect(response => {
+				proclaim.isString(response.text);
+				proclaim.match(
+					response.text,
+					/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-test-component@1\.0\.29&shrinkwrap=o-autoinit%401\.3\.3\n \*\//,
+				);
+			});
+	});
 
-test("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=off", async t => {
-	return request(this.app)
-		.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=off")
-		.expect(200)
-		.expect(
-			"cache-control",
-			"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-		)
-		.expect("Content-Type", "text/css; charset=utf-8")
-		.expect(response => {
-			proclaim.isString(response.text);
-			proclaim.include(
-				response.text,
-				`.test-compile-error {
-  color: red; }`,
-			);
-			proclaim.match(
-				response.text,
-				/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-test-component@1\.0\.29&shrinkwrap=o-autoinit%40.*\n \*\//,
-			);
-		});
+	it("GET /v3/bundles/css?modules=o-test-component@1.0.29&minify=off", async t => {
+		return request(this.app)
+			.get("/v3/bundles/css?modules=o-test-component@1.0.29&minify=off")
+			.expect(200)
+			.expect(
+				"cache-control",
+				"public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+			)
+			.expect("Content-Type", "text/css; charset=utf-8")
+			.expect(response => {
+				proclaim.isString(response.text);
+				proclaim.include(
+					response.text,
+					`.test-compile-error {
+	color: red; }`,
+				);
+				proclaim.match(
+					response.text,
+					/\/\*\* Shrinkwrap URL:\n \*      \/v3\/bundles\/css\?modules=o-test-component@1\.0\.29&shrinkwrap=o-autoinit%40.*\n \*\//,
+				);
+			});
+	});
 });
