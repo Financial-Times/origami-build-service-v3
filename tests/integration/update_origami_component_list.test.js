69a4464a09285a8c54dda8b460a354b105f81fff
Jake Champion <jakechampion.jake2@googlemail.com>
2019-09-19 14:56:19 +0100

    Add integration test

---
+++ tests/integration/update_origami_component_list.test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const request = require("supertest");
+const process = require("process");
+const tap = require("tap");
+
+const HOST = process.env.HOST;
+tap.test(
+  "It should response the GET method with a 200 HTTP status code",
+  async t => {
+    const response = await request(HOST).get(
+      "/v3/update_origami_component_list"
+    );
+    t.strictSame(response.statusCode, 200);
+  }
+);
