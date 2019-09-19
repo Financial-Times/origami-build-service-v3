3d2fae50cf42565a877638904a64918085a61542
Jake Champion <jakechampion.jake2@googlemail.com>
2019-09-19 13:54:13 +0100

    remove hello function

--- functions/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const Raven = require("raven");
-const RavenLambdaWrapper = require("serverless-sentry-lib");
-const { hello } = require("../src/hello");
-
-exports.hello = RavenLambdaWrapper.handler(Raven, async () => {
-  return hello();
-});
