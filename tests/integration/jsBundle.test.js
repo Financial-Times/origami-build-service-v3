acc1b84448c85d38f6020ee11197957641c73969
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-07 13:29:08 +0000

    Add tests for js bundling

--- tests/integration/jsBundle.test.js
+++ tests/integration/jsBundle.test.js
@@ -9,38 +9,30 @@ const isES6 = require("is-es6-syntax");
 const isES7 = require("is-es7-syntax");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const fs = require("fs");
-const fetchPolyfill = fs.readFileSync(require.resolve("whatwg-fetch"), "utf-8");
+const { Script } = require("vm");
 
 const doesNotThrowInBrowserEnvironment = js => {
-  const window = new JSDOM(``, { runScripts: "outside-only" }).window;
-  proclaim.doesNotThrow(
-    () => {
-      try {
-        window.eval(fetchPolyfill);
-        window.eval(js);
-      } catch (err) {
-        console.error(err);
-        throw err;
-      }
-    },
-    undefined,
-    `Expected to be valid browser-based JavaScript but it was not.`,
-  );
+  const dom = new JSDOM(``, { runScripts: "outside-only" });
+  const script = new Script(js);
 
-  return window;
+  proclaim.doesNotThrow(() => {
+    try {
+      dom.runVMScript(script);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }, `Expected to be valid browser-based JavaScript but it was not.`);
+
+  return dom.window;
 };
 
 const doesThrowInBrowserEnvironment = (js, message) => {
-  const window = new JSDOM(``, { runScripts: "outside-only" }).window;
-  proclaim.throws(
-    () => {
-      window.eval(fetchPolyfill);
-      window.eval(js);
-    },
-    undefined,
-    message,
-  );
+  const dom = new JSDOM(``, { runScripts: "outside-only" });
+  const script = new Script(js);
+  proclaim.throws(() => {
+    dom.runVMScript(script);
+  }, message);
 };
 
 const HOST = process.env.HOST;
@@ -48,213 +40,240 @@ const HOST = process.env.HOST;
 describe("/v3/bundles/js", function() {
   context("missing all parameters", function() {
     it("GET /v3/bundles/js", async function() {
-      return request(HOST)
-        .get("/v3/bundles/js")
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter is required",
-          );
-        });
+      const response = await request(HOST).get("/v3/bundles/js");
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter is required.",
+      );
     });
   });
 
   context("invalid modules parameter", function() {
-    it("GET /v3/bundles/js?modules", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules")
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter can not be empty",
-          );
-        });
+    it("GET /v3/bundles/js?modules", async function() {
+      const response = await request(HOST).get("/v3/bundles/js?modules");
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter is required.",
+      );
     });
 
-    it("GET /v3/bundles/js?modules=,,", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=,,")
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter can not be empty",
-          );
-        });
+    it("GET /v3/bundles/js?modules=,,", async function() {
+      const response = await request(HOST).get("/v3/bundles/js?modules=,,");
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter can not be empty.",
+      );
     });
 
-    it("GET /v3/bundles/js?modules=1a-", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=1a-")
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter contains module names which are not valid: 1a-",
-          );
-        });
+    it("GET /v3/bundles/js?modules=!1", async function() {
+      const response = await request(HOST).get(`/v3/bundles/js?modules=!1`);
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter contains module names which are not valid: !1.",
+      );
     });
   });
 
-  context("missing source parameter", function() {
-    it("returns an error", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=o-test-component")
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "Missing source query parameter, the value should be a valid biz-ops systemcode.",
-          );
-        });
+  context.skip("missing source parameter", function() {
+    it("returns an error", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-date@*",
+      );
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "Missing source query parameter, the value should be a valid biz-ops systemcode.",
+      );
     });
   });
 
   context("basic request", function() {
-    it("GET /v3/bundles/js?modules=@financial-times/o-date@*&source=test", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=@financial-times/o-date@*&source=test")
-        .expect(200)
-        .expect(
-          "cache-control",
-          "public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-        )
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(response => {
-          const window = doesNotThrowInBrowserEnvironment(response.text);
-          proclaim.include(window.Origami, "@financial-times/o-date");
-        });
+    it("GET /v3/bundles/js?modules=@financial-times/o-date@*&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-date@*&source=test",
+      );
+      proclaim.deepEqual(response.statusCode, 200);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      const window = doesNotThrowInBrowserEnvironment(response.text);
+      proclaim.include(window.Origami, "@financial-times/o-date");
     });
   });
 
   context("requesting the same module multiple times", function() {
-    it("GET /v3/bundles/js?modules=@financial-times/o-date@*,@financial-times/o-date@*", function() {
-      return request(HOST)
-        .get(
-          "/v3/bundles/js?modules=@financial-times/o-date@*,@financial-times/o-date@*",
-        )
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter contains duplicate module names.",
-          );
-        });
+    it("GET /v3/bundles/js?modules=@financial-times/o-date@*,@financial-times/o-date@*", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-date@*,@financial-times/o-date@*",
+      );
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter contains duplicate module names.",
+      );
     });
 
-    it("GET /v3/bundles/js?modules=@financial-times/o-test-component@1.0.19,@financial-times/o-test-component@1.0.17%20-%201.0.19", function() {
-      return request(HOST)
-        .get(
-          "/v3/bundles/js?modules=@financial-times/o-test-component@1.0.19,@financial-times/o-test-component@1.0.17%20-%201.0.19",
-        )
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter contains duplicate module names.",
-          );
-        });
+    it("GET /v3/bundles/js?modules=@financial-times/o-test-component@1.0.19,@financial-times/o-test-component@1.0.17%20-%201.0.19", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-test-component@1.0.19,@financial-times/o-test-component@1.0.17%20-%201.0.19",
+      );
+
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter contains duplicate module names.",
+      );
     });
 
-    it("GET /v3/bundles/js?modules=@financial-times/o-test-component@1.0.17,@financial-times/o-test-component@1.0.19", function() {
-      return request(HOST)
-        .get(
-          "/v3/bundles/js?modules=@financial-times/o-test-component@1.0.17,@financial-times/o-test-component@1.0.19",
-        )
-        .expect(400)
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(
-          "cache-control",
-          "max-age=0, must-revalidate, no-cache, no-store",
-        )
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            "The modules query parameter contains duplicate module names.",
-          );
-        });
+    it("GET /v3/bundles/js?modules=@financial-times/o-test-component@1.0.17,@financial-times/o-test-component@1.0.19", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-test-component@1.0.17,@financial-times/o-test-component@1.0.19",
+      );
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter contains duplicate module names.",
+      );
     });
   });
 
-  context("requesting two different modules", function() {
-    it("GET /v3/bundles/js?modules=@financial-times/o-autoinit@1.5,@financial-times/o-date@*&source=test", function() {
-      return request(HOST)
-        .get(
-          "/v3/bundles/js?modules=@financial-times/o-autoinit@1.5,@financial-times/o-date@*&source=test",
-        )
-        .expect(200)
-        .expect(
-          "cache-control",
-          "public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
-        )
-        .expect("Content-Type", "application/javascript;charset=UTF-8")
-        .expect(response => {
-          const window = doesNotThrowInBrowserEnvironment(response.text);
-          proclaim.include(window.Origami, "@financial-times/o-date");
-          proclaim.include(window.Origami, "@financial-times/o-autoinit");
-        });
+  context("requesting two different modules", async function() {
+    it("GET /v3/bundles/js?modules=@financial-times/o-autoinit@1.5,@financial-times/o-date@*&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-autoinit@1.5,@financial-times/o-date@*&source=test",
+      );
+
+      proclaim.deepEqual(response.statusCode, 200);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "public, max-age=86400, stale-if-error=604800, stale-while-revalidate=300000",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      const window = doesNotThrowInBrowserEnvironment(response.text);
+      proclaim.include(window.Origami, "@financial-times/o-date");
+      proclaim.include(window.Origami, "@financial-times/o-autoinit");
     });
   });
 
   context("invalid module name", function() {
-    it("GET /v3/bundles/js?modules=o-autoinit_±-test&source=test", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=o-autoinit_±-test&source=test")
-        .expect(400)
-        .expect(response => {
-          doesThrowInBrowserEnvironment(
-            response.text,
-            'The modules query parameter contains module names which are not valid: "o-autoinit_±-test"',
-          );
-        });
+    it("GET /v3/bundles/js?modules=o-autoinit_±-test&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=o-autoinit_±-test&source=test",
+      );
+      proclaim.deepEqual(response.statusCode, 400);
+      proclaim.deepEqual(
+        response.headers["cache-control"],
+        "max-age=0, must-revalidate, no-cache, no-store",
+      );
+      proclaim.deepEqual(
+        response.headers["content-type"],
+        "application/javascript;charset=UTF-8",
+      );
+      // TODO: Is this a potential XSS?
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "The modules query parameter contains module names which are not valid: o-autoinit_±-test.",
+      );
+    });
+  });
+
+  context("module which does not exist", function() {
+    it("GET /v3/bundles/js?modules=o-jake-does-not-exist&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=o-jake-does-not-exist&source=test",
+      );
+      proclaim.deepEqual(response.statusCode, 400);
+      doesThrowInBrowserEnvironment(
+        response.text,
+        "Because o-bundle depends on o-jake-does-not-exist which doesn't exist (could not find package o-jake-does-not-exist), version solving failed.\n",
+      );
     });
   });
 
   context("attaches modules to the Origami global object", function() {
-    it("GET /v3/bundles/js?modules=@financial-times/o-date@*&source=test", function() {
-      return request(HOST)
-        .get("/v3/bundles/js?modules=@financial-times/o-date@*&source=test")
-        .expect(response => {
-          const window = doesNotThrowInBrowserEnvironment(response.text);
-          proclaim.include(window.Origami, "@financial-times/o-date");
-        });
+    it("GET /v3/bundles/js?modules=@financial-times/o-date@*&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=@financial-times/o-date@*&source=test",
+      );
+
+      const window = doesNotThrowInBrowserEnvironment(response.text);
+      proclaim.include(window.Origami, "@financial-times/o-date");
     });
   });
 
