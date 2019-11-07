f7bcb6b587ed80d59cc45508a464366bb77b501c
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-07 14:16:47 +0000

    Add nice errors if any of the requested versions are invalid semver

--- src/modules/create-package-json-file.js
+++ src/modules/create-package-json-file.js
@@ -2,6 +2,8 @@
 
 const fs = require("fs").promises;
 const path = require("path");
+const { FormatException } = require("./HOME");
+const { VersionConstraint } = require("./Version");
 
 /**
  * Creates a package.json file within `bundleLocation` with the `modules` as the dependencies.
@@ -10,6 +12,29 @@ const path = require("path");
  * @returns {Promise<void>}
  */
 module.exports = async function createPackageJsonFile(bundleLocation, modules) {
+  const errors = [];
+  modules.reduce(function reducer(reduction, value, key) {
+    try {
+      VersionConstraint.parse(value);
+    } catch (e) {
+      if (e instanceof FormatException) {
+        reduction.push(
+          `The version ${value} in ${key}@${value} is not a valid version.`,
+        );
+      }
+    }
+
+    return reduction;
+  }, errors);
+
+  if (errors.length > 0) {
+    throw new FormatException(
+      errors.join("\n") +
+        "\n" +
+        "Please refer to TODO (build service documentation) for what is a valid version.",
+    );
+  }
+
   await fs.writeFile(
     path.join(bundleLocation, "./package.json"),
     JSON.stringify({
