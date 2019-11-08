678b2d4c8e8a865b2e82e1e98eda7cc561bb75f8
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-08 14:51:39 +0000

    Remove jest and tap, use mocha for unit tests

--- tests/unit/src/modules/getProductionCodeFor.test.js
+++ tests/unit/src/modules/getProductionCodeFor.test.js
@@ -1,64 +1,63 @@
+/* eslint-env mocha */
 "use strict";
 
-const tap = require("tap");
 const mockery = require("mockery");
 const sinon = require("sinon");
+const proclaim = require("proclaim");
 
-tap.test("getProductionCodeFor", t => {
-  t.beforeEach((done, t) => {
+describe("getProductionCodeFor", () => {
+  let execaStub;
+  let decompressStub;
+  let rimrafStub;
+  let tarStub;
+  let fsStub;
+  let getProductionCodeFor;
+
+  beforeEach(() => {
     mockery.enable({
       useCleanCache: true,
       warnOnReplace: false,
       warnOnUnregistered: false,
     });
-    t.context.execaStub = { command: sinon.stub() };
-    mockery.registerMock("execa", t.context.execaStub);
-    t.context.decompressStub = sinon.stub();
-    mockery.registerMock("decompress", t.context.decompressStub);
-    t.context.rimrafStub = sinon.stub();
-    mockery.registerMock("rimraf", t.context.rimrafStub);
-    t.context.tarStub = { c: sinon.stub() };
-    mockery.registerMock("tar", t.context.tarStub);
-    t.context.fsStub = {
+    execaStub = { command: sinon.stub() };
+    mockery.registerMock("execa", execaStub);
+    decompressStub = sinon.stub();
+    mockery.registerMock("decompress", decompressStub);
+    rimrafStub = sinon.stub();
+    mockery.registerMock("rimraf", rimrafStub);
+    tarStub = { c: sinon.stub() };
+    mockery.registerMock("tar", tarStub);
+    fsStub = {
       mkdtemp: sinon.stub().resolves("/temp-test-directory"),
       mkdir: sinon.stub().resolves(),
       readFile: sinon.stub().resolves(new ArrayBuffer()),
     };
-    mockery.registerMock("fs", { promises: t.context.fsStub });
-    t.context.getProductionCodeFor = require("../../../../src/modules/getProductionCodeFor");
-    done();
+    mockery.registerMock("fs", { promises: fsStub });
+    getProductionCodeFor = require("../../../../src/modules/getProductionCodeFor");
   });
 
-  t.afterEach(done => {
+  afterEach(() => {
     mockery.disable();
-    done();
   });
 
-  t.test("it is a function", async t => {
-    t.type(t.context.getProductionCodeFor, "function");
-    t.end();
+  it("it is a function", async () => {
+    proclaim.isFunction(getProductionCodeFor);
   });
 
-  t.test(
-    "downloads a tarball from npm with the name and version given",
-    async t => {
-      t.context.execaStub.command.resolves({
-        stdout: new ArrayBuffer(),
-      });
-      t.context.decompressStub.resolves();
-      t.context.rimrafStub.yields();
-      await t.context.getProductionCodeFor("@financial-times/o-test", "1.2.3");
-      t.true(
-        t.context.execaStub.command.calledOnceWith(
-          `npm pack @financial-times/o-test@'1.2.3'`,
-          {
-            shell: true,
-          },
-        ),
-      );
-      t.end();
-    },
-  );
-
-  t.end();
+  it("downloads a tarball from npm with the name and version given", async () => {
+    execaStub.command.resolves({
+      stdout: new ArrayBuffer(),
+    });
+    decompressStub.resolves();
+    rimrafStub.yields();
+    await getProductionCodeFor("@financial-times/o-test", "1.2.3");
+    proclaim.isTrue(
+      execaStub.command.calledOnceWith(
+        `npm pack @financial-times/o-test@'1.2.3'`,
+        {
+          shell: true,
+        },
+      ),
+    );
+  });
 });
