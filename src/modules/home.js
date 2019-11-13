7852a324e78a41087b93f7816f17f3ac23288e68
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-13 11:44:23 +0000

    compare numbers using === instead of immutable.is

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const { is } = require("immutable");
-
 /**
  * Returns whether `version1` is the same as `version2`, ignoring their prereleases.
  * @param {import('./version').Version} version1
@@ -9,8 +7,8 @@ const { is } = require("immutable");
  * @returns {boolean}
  */
 const equalsWithoutPreRelease = (version1, version2) =>
-  is(version1.major, version2.major) &&
-  is(version1.minor, version2.minor) &&
-  is(version1.patch, version2.patch);
+  version1.major === version2.major &&
+  version1.minor === version2.minor &&
+  version1.patch === version2.patch;
 
 module.exports.equalsWithoutPreRelease = equalsWithoutPreRelease;
