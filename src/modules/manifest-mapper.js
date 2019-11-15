16694ec53a809c15283f740ebc7514cc4825d83e
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-15 15:06:56 +0000

    Add support for dynamodb to be run locally rather than on aws to help aid development

--- src/modules/manifest-mapper.js
+++ src/modules/manifest-mapper.js
@@ -3,11 +3,23 @@
 let { DataMapper } = require("@aws/dynamodb-data-mapper");
 let DynamoDB = require("aws-sdk/clients/dynamodb");
 let process = require("process");
-const mapper = new DataMapper({
-  client: new DynamoDB({
+
+let client;
+
+if (process.env.IS_OFFLINE) {
+  client = new DynamoDB({
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+  });
+} else {
+  client = new DynamoDB({
     apiVersion: "2012-08-10",
     region: process.env.AWS_REGION,
-  }),
+  });
+}
+
+const mapper = new DataMapper({
+  client,
 });
 
 module.exports.mapper = mapper;
