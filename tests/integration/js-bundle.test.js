d275894a858de1bf9602e2f49953f16e3a78a3f4
Jake Champion <jakechampion.jake2@googlemail.com>
2019-12-09 14:29:25 +0000

    Add test for component who has dependency defined in package.json who version is not a string

--- tests/integration/js-bundle.test.js
+++ tests/integration/js-bundle.test.js
@@ -295,6 +295,20 @@ describe("/v3/bundles/js", function() {
     });
   });
 
+  context("module which has invalid version for a dependency", function() {
+    it("GET /v3/bundles/js?modules=component-with-invalid-dependency-version@1.0.0&source=test", async function() {
+      const response = await request(HOST).get(
+        "/v3/bundles/js?modules=component-with-invalid-dependency-version@1.0.0&source=test",
+      );
+      proclaim.deepEqual(response.statusCode, 500);
+      doesThrowInBrowserEnvironment(
+        response.text,
+        // TODO: Is this a potential XSS?
+        'Origami Build Service returned an error: component-with-invalid-dependency-version@1.0.0: The manifest\'s "dependencies" field has an entry for "component-with-no-dependencies" which is not a string. Dependencies can only be defined with SemVer strings. The manifest is "{\n    "name": "component-with-invalid-dependency-version",\n    "version": "1.0.0",\n    "dependencies": {\n        "component-with-no-dependencies": true\n    }\n}".',
+      );
+    });
+  });
+
   context("version which does not exist", function() {
     it("GET /v3/bundles/js?modules=component-with-no-dependencies@1111111&source=test", async function() {
       const response = await request(HOST).get(
