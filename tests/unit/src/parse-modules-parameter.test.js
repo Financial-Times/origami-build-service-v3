dba12fb9099e5095e08b6343f8b671907c8c9d81
Jake Champion <jakechampion.jake2@googlemail.com>
2019-12-11 13:44:02 +0000

    Assert that the expected error messages are given

--- tests/unit/src/parse-modules-parameter.test.js
+++ tests/unit/src/parse-modules-parameter.test.js
@@ -14,23 +14,39 @@ describe("parseModulesParameter", () => {
     proclaim.throws(() => {
       parseModulesParameter();
     }, UserError);
+
+    proclaim.throws(() => {
+      parseModulesParameter();
+    }, "The modules query parameter is required.");
   });
 
   it("throws UserError if modules parameter is empty string", async () => {
     proclaim.throws(() => {
       parseModulesParameter("");
     }, UserError);
+
+    proclaim.throws(() => {
+      parseModulesParameter("");
+    }, "The modules query parameter is required.");
   });
 
   it("throws UserError if modules parameter contains duplicates", async () => {
     proclaim.throws(() => {
       parseModulesParameter("o-test@1,o-test@1");
     }, UserError);
+
+    proclaim.throws(() => {
+      parseModulesParameter("o-test@1,o-test@1");
+    }, "The modules query parameter contains duplicate module names.");
   });
 
   it("throws UserError if modules parameter contains empty module names", async () => {
     proclaim.throws(() => {
       parseModulesParameter("o-test@1,,");
     }, UserError);
+
+    proclaim.throws(() => {
+      parseModulesParameter("o-test@1,,");
+    }, "The modules query parameter can not contain empty module names.");
   });
 });
